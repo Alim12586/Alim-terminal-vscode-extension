2d7cfae133e6f0ea96a00056064633b42ed96da0
Alim12586 <alimtas12@gmail.com>
2025-10-29 11:07:49 +0300

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,27 @@
+const vscode = require('vscode');
+const { exec } = require('child_process');
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand('alimTerminal.runPhone', function () {
+    const terminal = vscode.window.createTerminal('Alim Terminal');
+    terminal.show();
+    terminal.sendText('echo "📱 Alim New Phone başlatılıyor..."');
+
+    // Flutter projesi çalıştır
+    terminal.sendText('cd flutter_project && flutter run');
+
+    // HTML projesi için basit sunucu
+    terminal.sendText('cd html_project && npx serve');
+
+    vscode.window.showInformationMessage('Alim New Phone komutu çalıştırıldı!');
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
